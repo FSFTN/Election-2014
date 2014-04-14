d3ccc90c712d7a187d7ff7c7c2fdf3025b1ce3ed
rajanand02 <rajanand0202@gmail.com>
2014-04-14 17:04:41 +0530

    add server details and setup production env

--- Gemfile
+++ Gemfile
@@ -5,6 +5,7 @@ gem 'rails', '4.1.0'
 gem 'sqlite3'
 group :production do
  gem 'pg' 
+ gem 'rails_12factor'
 end
 gem 'sass-rails', '~> 4.0.3'
 gem 'uglifier', '>= 1.3.0'
--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,3 +1,5 @@
+set :stage, :production
+set :branch, proc {'develop'}
 # Simple Role Syntax
 # ==================
 # Supports bulk-adding hosts to roles, the primary
@@ -14,7 +16,7 @@ role :db,  %w{deploy@example.com}
 # definition into the server list. The second argument
 # something that quacks like a hash can be used to set
 # extended properties on the server.
-server 'example.com', user: 'deploy', roles: %w{web app}, my_property: :my_value
+server '107.170.250.190', user: 'fsftn', roles: %w{web app}, my_property: :my_value
 
 # you can set custom ssh options
 # it's possible to pass any option but you need to keep in mind that net/ssh understand limited list of options
